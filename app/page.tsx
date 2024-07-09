0321a9cd9c223ba6b2596b074a4e767e80934fb1
Laniran JohnPaul <jplaniran01@gmail.com>
2024-07-09 11:23:47 +0100

    no general padding for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { HomepageBundles, HomepageServices } from "@/components";
 import AppWrapper from "@/components/LandingPage/AppWrapper";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <AppWrapper type="">
-      <main className="flex flex-col items-center justify-between p-24">
+      <main className="flex flex-col items-center justify-between">
         <h1>Home page</h1>
+        <HomepageBundles />
+        <HomepageServices />
       </main>
     </AppWrapper>
   );
