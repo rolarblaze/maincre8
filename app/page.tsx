a25682b51902dceb31961b6391b1f71ef469a9eb
AJ1732 <ejemeniboi@gmail.com>
2024-07-09 13:01:10 +0100

    chore: import sections into home pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { HomepageBundles, HomepageServices } from "@/components";
 import AppWrapper from "@/components/LandingPage/AppWrapper";
+import BrandSection from "@/components/LandingPage/Sections/BrandSection";
+import DashboardSection from "@/components/LandingPage/Sections/DashboardSection";
+import HeroSection from "@/components/LandingPage/Sections/HeroSection";
 export default function Home() {
   return (
     <AppWrapper type="">
       <main className="flex flex-col items-center justify-between">
         <h1>Home page</h1>
-        <HomepageBundles />
+        <HeroSection />
         <HomepageServices />
+        <HomepageBundles />
+        <DashboardSection />
+        <BrandSection />
       </main>
     </AppWrapper>
   );
