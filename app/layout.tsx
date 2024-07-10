b41502e0ad7e4fe04aacc404e093b1a8c2984587
Precious_codes <Rukyjacob@gmail.com>
2024-07-10 19:29:42 +0100

    Revert "Bundles section updates and Benefits section"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Manrope({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,58 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      lineHeight: {
-        loosest: "5rem",
-      },
-      spacing: {
-        "4.5": "1.125rem",
-      },
-      borderRadius: {
-        "5xl": "1.875rem",
-      },
       backgroundImage: {
-        "blue-gradient":
-          "linear-gradient(to right bottom, #083160 0%, #083160 25%, #04182F 50%, #04182F 100%)",
-      },
-      fontSize: {
-        "3.5xl": "2rem",
-        "4.5xl": "2.5rem",
-        "5.5xl": "3.5rem",
-      },
-      colors: {
-        grey50: "#F9FAFB",
-        grey100: "#F0F2F5",
-        grey200: "#E4E7EC",
-        grey300: "#D0D5DD",
-        grey400: "#98A2B3",
-        grey500: "#667185",
-        grey600: "#475367",
-        grey800: "#1D2739",
-        grey900: "#101928",
-        primary400: "#1374E4",
-        primary500: "#1574E5",
-        primary700: "#0D5EBA",
-        primary600: "#136AD0",
-        primary800: "#083160",
-        primary900: "#072548",
-        "dark-cyan-blue": "#030E1C",
-      },
-      boxShadow: {
-        'custom-strong': '0 5px 10px rgba(0, 0, 0, 0.15), 0 -5px 10px rgba(0, 0, 0, 0.15), 5px 0 10px rgba(0, 0, 0, 0.15), -5px 0 10px rgba(0, 0, 0, 0.15)',
-      },
-      keyframes: {
-        fadeInDown: {
-          '0%': { opacity: '0', transform: 'translateY(-20px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
-        },
-        fadeOutUp: {
-          '0%': { opacity: '1', transform: 'translateY(0)' },
-          '100%': { opacity: '0', transform: 'translateY(-20px)' },
-        },
-      },
-      animation: {
-        fadeInDown: 'fadeInDown 0.6s ease-out',
-        fadeOutUp: 'fadeOutUp 0.8s ease-out',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
