a9afb0fc4cfd645b7a6a27828496b4be6e6c103e
AJ1732 <ejemeniboi@gmail.com>
2024-07-10 11:37:52 +0100

    chore: build honey comb layout

---
+++ components/HoneyCombGrid/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.css"
+
+const HoneyCombDisplay = () => {
+  return (
+    <div>
+      <ul className={styles.contain}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+export default HoneyCombDisplay;
