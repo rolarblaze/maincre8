5362dfa7ff83e73e6fd98dae9ad3072b5adf6366
AJ1732 <ejemeniboi@gmail.com>
2024-07-10 14:39:58 +0100

    chore: use images

--- components/HoneyCombGrid/index.tsx
+++ components/HoneyCombGrid/index.tsx
@@ -1,15 +1,23 @@
+import Image from "next/image"
 import styles from "./styles.module.css"
+import honeycomb1 from "@/public/imgs/honeycomb-1.jpg"
+import honeycomb2 from "@/public/imgs/honeycomb-2.jpg"
+import honeycomb3 from "@/public/imgs/honeycomb-3.jpg"
+import honeycomb4 from "@/public/imgs/honeycomb-4.jpg"
+import honeycomb5 from "@/public/imgs/honeycomb-5.jpg"
+import honeycomb6 from "@/public/imgs/honeycomb-6.jpg"
+
+const images = [honeycomb1, honeycomb2, honeycomb3, honeycomb4, honeycomb5, honeycomb6]
 
 const HoneyCombDisplay = () => {
   return (
     <div>
       <ul className={styles.contain}>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        {images.map((image, index) => (
+          <li key={index}>
+            <Image src={image} alt="Honey comb image"/>
+          </li>
+        ))}
       </ul>
     </div>
   );
