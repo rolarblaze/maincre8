27673224d375990c6d6451d1e38c3e49a11a11a6
AJ1732 <ejemeniboi@gmail.com>
2024-07-15 13:02:32 +0100

    chore: initialize shop route

---
+++ app/shop/page.tsx
@@ -0,0 +1,26 @@
+import { AppWrapper, HomepageCTA, HomepageSubscribe } from "@/components";
+
+const Shop = () => {
+  return (
+    <AppWrapper type="">
+      <div className="mt-20 space-y-10 max-w-[76rem] mx-auto">
+        {/* SEARCH AND FILTER SECTION */}
+        <div>SEARCH & FILTER INPUT</div>
+
+        {/* DIGITAL MARKETING SERVICES */}
+        <div>DMS SECTION</div>
+
+        {/* CREATIVE DESIGN SERVICES */}
+        <div>CDS SECTION</div>
+
+        {/* CONTENT/ COPYWRITING  SERVICES */}
+        <div>CCS SECTION</div>
+      </div>
+
+      {/* EXTRA SECTIONS */}
+      <HomepageCTA />
+      <HomepageSubscribe />
+    </AppWrapper>
+  );
+};
+export default Shop;
