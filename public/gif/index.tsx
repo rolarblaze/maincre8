d48753a5653d56fda39d3b2c373a37032bcc389c
AJ1732 <ejemeniboi@gmail.com>
2024-07-22 19:33:51 +0100

    chore: update gif elements and padding

--- public/gif/index.tsx
+++ public/gif/index.tsx
@@ -2,3 +2,4 @@ export { default as LaunchGif } from "./launch.gif";
 export { default as ColorPalette } from "./colour-palette.gif";
 export { default as AttachBrief } from "./attach-brief.gif";
 export { default as StartProject } from "./start-project.gif";
+export { default as IdeaBulb } from "./idea-bulb.gif";
