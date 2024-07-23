154afbe3b80bf584b2bf23dfe95d7e0b8907c531
AJ1732 <ejemeniboi@gmail.com>
2024-07-23 20:57:31 +0100

    chore: updtae responsiveness

--- components/LandingPage/Sections/Bundles/index.tsx
+++ components/LandingPage/Sections/Bundles/index.tsx
@@ -12,17 +12,17 @@ const Bundles = () => {
   };
 
   return (
-    <div className="min-h-screen bg-blue-gradient w-full py-20 px-28">
+    <div className="min-h-screen bg-blue-gradient w-full py-20 px-4 md:px-28">
       <div className="max-w-[1216px] mx-auto text-center relative">
-        <div className="absolute right-0">
+        <div className="absolute right-0 max-md:hidden">
           <Crea8Star />
         </div>
-        <h2 className="text-white">Our Top Bundles</h2>
+        <h2 className="text-white text-[clamp(1.5rem_,5vw,_3.5rem)]">Our Top Bundles</h2>
         <p className="text-grey50 max-w-[412px] mx-auto my-4">
           Hiring a world-class team of creative geniuses doesn't have to be
           expensive or complicated.
         </p>
-        <div className="absolute left-0 bottom-2">
+        <div className="absolute left-0 bottom-2 max-md:hidden">
           <Crea8Star />
         </div>
 
@@ -45,7 +45,7 @@ const Bundles = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-8 max-w-[1216px] mx-auto pt-8">
+      <div className="flex gap-8 md:max-w-[1216px] mx-auto w-full pt-8 overflow-x-scroll ">
         {serviceBundles[activeTab].map((service, index) => (
           <Card
             key={index}
