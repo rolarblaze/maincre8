a67bba3400aca3fe79f9439863b28a216b78d7a5
Laniran JohnPaul <jplaniran01@gmail.com>
2024-08-05 09:18:52 +0100

    fixed redux issues

--- redux/provider.tsx
+++ redux/provider.tsx
@@ -1,12 +1,8 @@
 "use client";
 import React from "react";
-import { store } from "./store";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export function GlobalProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
