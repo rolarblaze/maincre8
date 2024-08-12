d7954845a8c579dd6f2acffb38c9a119eae147c8
AJ1732 <ejemeniboi@gmail.com>
2024-08-12 23:44:31 +0100

    chore: create custom page

---
+++ app/dashboard/custom-recommendation/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components";
+
+const CustomRecommendation = () => {
+  return (
+    <div>
+      <Button label="Get new recommendation" classNames="max-w-[16.125rem]"/>
+
+      <div>Recommendations</div>
+    </div>
+  );
+};
+export default CustomRecommendation;
--- public/svgs/index.tsx
+++ public/svgs/index.tsx
@@ -35,4 +35,5 @@ export { default as BlueProjectIcon } from "./BlueProjectIcon";
 export { default as BlueSupportIcon } from "./BlueSupportIcon";
 export { default as BlueTargetIcon } from "./BlueTargetIcon";
 export { default as BlueUserIcon } from "./BlueUserIcon";
+export { default as BulbIcon } from "./BulbIcon";
 
