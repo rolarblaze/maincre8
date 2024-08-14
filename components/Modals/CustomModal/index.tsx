274dd5178262bde1cfb050014d219b815614316d
ODINKS <nicholas.okeke87@gmail.com>
2024-08-14 23:44:01 +0100

    fix

--- components/Modals/CustomModal/index.tsx
+++ components/Modals/CustomModal/index.tsx
@@ -19,7 +19,7 @@ const Modal: React.FC<ModalProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-5">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
       <div
         className="fixed inset-0 bg-black opacity-50"
         onClick={onClose}
