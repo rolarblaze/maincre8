90a9bc636b9b07759ea877a820713d4b3f7a5775
PreciousOritsedere <rukyjacob@gmail.com>
2024-08-19 19:39:18 +0100

    feat: latest appointment

--- redux/order/interface.ts
+++ redux/order/interface.ts
@@ -1,35 +1,47 @@
 
 export interface Service {
-    service_name: string;
-    service_id: number;
-  }
-  
-  export interface Bundle {
-    bundle_name: string;
-    bundle_id: number;
-    service: Service;
-  }
-  
-  export interface Package {
-    package_name: string;
-    package_id: number;
-    price: number | null;
-    description: string;
-    bundle: Bundle;
-  }
-  
-  export interface UserTransaction {
-    transaction_id: number;
-    trans_ref: string;
-    amount: number;
-    currency: string;
-    status: string;
-    package: Package;
-    created_at: string;
-    updated_at: string;
-  }
-  
-  export interface OrderHistoryResponse {
-    user_transactions: UserTransaction[];
-  }
-  
+  service_name: string;
+  service_id: number;
+}
+
+export interface Bundle {
+  bundle_name: string;
+  bundle_id: number;
+  service: Service;
+}
+
+export interface Package {
+  package_name: string;
+  package_id: number;
+  price: number | null;
+  description: string;
+  bundle: Bundle;
+}
+
+export interface UserTransaction {
+  transaction_id: number;
+  trans_ref: string;
+  amount: number;
+  currency: string;
+  status: string;
+  package: Package;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface OrderHistoryResponse {
+  user_transactions: UserTransaction[];
+}
+
+export interface Appointment {
+  event_name: string;
+  event_date: string;
+  product_name: string;
+}
+
+// export interface AppointmentResponse {
+//   appointments: Appointment[];
+// }
+
+export type AppointmentResponse = Appointment[];
+
