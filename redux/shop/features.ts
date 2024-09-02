9e6326a143a33f16b77197717b8052bf74515bbb
Laniran JohnPaul <jplaniran01@gmail.com>
2024-09-02 16:19:07 +0100

    infinite scroll for packages

--- redux/shop/features.ts
+++ redux/shop/features.ts
@@ -1,20 +1,33 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "@/utils/axios/api";
 import { handleAxiosError } from "@/utils/helpers/general/errorHandler";
-import { Service } from "./interface";
+import { Package, Packages, Service } from "./interface";
 
 // Fetch services
 export const getServices = createAsyncThunk<Service[], void>(
-    "shop/getServices",
-    async (_, { rejectWithValue }) => {
-        try {
-            const response = await api.get("admin-user/services");
-            return response.data.services;
-        } catch (error) {
-            console.error("Error fetching services:", error);
-            return rejectWithValue(handleAxiosError(error));
-        }
+  "shop/getServices",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await api.get("admin-user/services");
+      return response.data.services;
+    } catch (error) {
+      console.error("Error fetching services:", error);
+      return rejectWithValue(handleAxiosError(error));
     }
+  }
 );
 
-
+export const getPackages = createAsyncThunk<
+  Package[],
+  { limit: number; offset: number }
+>("shop/getPackages", async ({ limit, offset }, { rejectWithValue }) => {
+  try {
+    const response = await api.get(
+      `user/packages?limit=${limit}&offset=${offset}`
+    );
+    return response.data.packages;
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    return rejectWithValue(handleAxiosError(error));
+  }
+});
