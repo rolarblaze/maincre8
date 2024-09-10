29ecae717ea7e3d68a74ff1b57a4afb087933608
ODINKS <nicholas.okeke87@gmail.com>
2024-09-10 19:50:29 +0100

    chore: move country code length to a helper function

--- components/PhoneInput/index.tsx
+++ components/PhoneInput/index.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
+import { countryLocalPhoneLengths } from "./utils/countryCodes";
 
 interface PhoneNumberInputProps {
   value: string;
@@ -19,34 +20,7 @@ const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
   const [error, setError] = useState("");
   const [countryInfo, setCountryInfo] = useState<any>(null); // State to store the country info
 
-  // Country codes with their respective local phone number lengths (excluding country code)
-  const countryLocalPhoneLengths: { [key: string]: number } = {
-    us: 10, // Local number is 10 digits
-    ca: 10,
-    gb: 10,
-    au: 9,
-    de: 11,
-    fr: 9,
-    it: 10,
-    es: 9,
-    in: 10,
-    cn: 11,
-    br: 10,
-    ng: 10, // Nigeria's local number is 10 digits
-    za: 9,
-    jp: 10,
-    mx: 10,
-    ru: 10,
-    ar: 10,
-    eg: 10,
-    sa: 9,
-    kr: 10,
-    id: 11,
-    tr: 10,
-    ph: 10,
-    pk: 10,
-  };
-
+  
   // Replace the handleChange with this function
   const handlePhoneChange = (phone: string, country: any) => {
     setCountryInfo(country); // Store country info when phone number changes
