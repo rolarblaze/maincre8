b6f55aa29ede87a8d18dbc590820627375e822df
ODINKS <nicholas.okeke87@gmail.com>
2024-09-20 22:05:39 +0100

    design: refactor the what-customers-say section

--- components/LandingPage/Sections/CustomerReviews/Components/ReviewData.tsx
+++ components/LandingPage/Sections/CustomerReviews/Components/ReviewData.tsx
@@ -5,24 +5,24 @@ import avatar3 from "@/public/imgs/avatar3.png";
 const Reviews = [
   {
     review:
-      "SellCrea8 transformed our brand identity with their creative services. Highly recommended!",
+      '"SellCrea8 transformed our brand identity with their creative services. Highly recommended!"',
     src: avatar1,
     name: "Elizabeth P",
-    role: "Creative Director",
+    role: "Founder, Diary of a Naija Girl",
   },
   {
     review:
-      "Their digital marketing strategy helped us reach a wider audience and increase sales.",
+      '"Their digital marketing strategy helped us reach a wider audience and increase sales."',
     src: avatar2,
     name: "Eze J",
-    role: "Art Director",
+    role: "CEO, Jobberman",
   },
   {
     review:
-      "Fantastic support and easy project management. SellCrea8 is our go-to platform for creative needs.",
+      '"Fantastic support and easy project management. SellCrea8 is our go-to platform for creative needs."',
     src: avatar3,
     name: "Nicholas T",
-    role: "Brand Manager",
+    role: "Head of Marketing, ",
   },
 ];
 
