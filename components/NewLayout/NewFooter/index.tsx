15faffd9cee569cafd2757702149aa1053a9946c
AJ1732 <ejemeniboi@gmail.com>
2024-09-22 19:10:17 +0100

    chore: update colors

--- components/NewLayout/NewFooter/index.tsx
+++ components/NewLayout/NewFooter/index.tsx
@@ -1,4 +1,8 @@
 const NewFooter = () => {
-  return <footer>NewFooter</footer>;
+  return (
+    <footer className="full-width content-grid py-[6.25rem] text-grey200 bg-primary900">
+      <section>NewFooter</section>
+    </footer>
+  );
 };
 export default NewFooter;
