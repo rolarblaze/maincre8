bc80dff07a5f1e71195f3a4180e89c5a4c7cf0f6
AJ1732 <ejemeniboi@gmail.com>
2024-09-28 00:47:11 +0100

    chore: update

--- components/NewLayout/index.tsx
+++ components/NewLayout/index.tsx
@@ -5,7 +5,7 @@ const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="full-width content-grid place-content-start font-manrope">
       <NewNavbar />
-      <main className="full-width content-grid mt-[5.5rem] min-h-[calc(100dvh-4rem)]">
+      <main className="full-width content-grid mt-[5.5rem] min-h-[calc(100dvh-4rem)] place-content-start">
         {children}
       </main>
       <NewFooter />
--- components/NewPages/PrivacyPolicyPage/sections/ContentSection/index.tsx
+++ components/NewPages/PrivacyPolicyPage/sections/ContentSection/index.tsx
@@ -4,12 +4,12 @@ import { RootState } from "@/redux/store";
 
 const ContentSection = () => {
   const { activeTab } = useSelector((state: RootState) => state.tabs);
-  
+
   console.log(activeTab);
-  
+
   return (
-    <section className="full-width content-grid bg-grey50 py-[6.25rem]">
-      <div className="mx-auto min-h-80 w-full max-w-3xl">
+    <section className="full-width content-grid min-h-96 bg-grey50 py-[6.25rem]">
+      <div className="mx-auto w-full max-w-3xl">
         <p>Active Tab: {activeTab}</p>
       </div>
     </section>
