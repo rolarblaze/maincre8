ed3e2ecaa1804a7c6a010dac994fdc7bde501a0a
ODINKS <nicholas.okeke87@gmail.com>
2024-11-05 11:53:24 +0100

    chore: creat graphics design form schema

--- components/Dashboard/SubmitBrief/shared/formTypes/graphicsDesign.ts
+++ components/Dashboard/SubmitBrief/shared/formTypes/graphicsDesign.ts
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+
 export interface GraphicsDesignValues {
   graphicsType: string[];
   graphicsColorPalette: string;
@@ -19,3 +21,34 @@ export const graphicsDesignValues = {
   graphicsReferences: "",
   graphicsTimeline: [],
 };
+
+export const graphicsDesignFormSchema = Yup.object().shape({
+  // SELECT FIELDS
+  graphicsType: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one graphics type is required")
+    .required("Please select a graphics type"),
+  graphicsColorPalette: Yup.string().required("Please input a color palette"),
+  graphicsTargetAud: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one target audience opion is required")
+    .required("Please select a target audience"),
+  graphicsPrimaryMessage: Yup.string().required("Please input a content tone"),
+  graphicsTone: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one graphics tone option is required")
+    .required("Please select a graphics tone option"),
+
+  graphicsDeliverable: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one deliverable option is required")
+    .required("Please select a deliverable option"),
+  graphicsReferences: Yup.string().required("Please input a reference."),
+  graphicsTimeline: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one timeline option is required")
+    .required("Please select a timeline option"),
+
+  // ATTACH A FILE
+  document: Yup.mixed().nullable().optional(),
+});
