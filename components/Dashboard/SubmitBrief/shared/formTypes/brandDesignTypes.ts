7012de7edcc13f0b550715c54ae88a9ad40544b7
ODINKS <nicholas.okeke87@gmail.com>
2024-11-05 11:27:57 +0100

    chore: creat brand design form schema

--- components/Dashboard/SubmitBrief/shared/formTypes/brandDesignTypes.ts
+++ components/Dashboard/SubmitBrief/shared/formTypes/brandDesignTypes.ts
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+
 export interface BrandDesignValues {
   brandCoreValue: string;
   brandMarket: string[];
@@ -19,3 +21,35 @@ export const brandDesignInitialValues = {
   brandCompetitors: "",
   brandGuidelines: "",
 };
+
+export const digitalMarketingFormSchema = Yup.object().shape({
+  // SELECT FIELDS
+  brandCoreValue: Yup.string().required("At Please input a core value"),
+  brandMarket: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one brand market option is required")
+    .required("Please select a brand market"),
+  brandPersonality: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one brand personality option is required")
+    .required("Please select a brand personality"),
+  brandAsset: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one brand asset opion is required")
+    .required("Please select a brand asset"),
+  brandDeliverable: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one brand deliverable option is required")
+    .required("Please select a brand deliverable option"),
+  brandKPI: Yup.array()
+    .of(Yup.string().required())
+    .min(1, "At least one brand KPI option is required")
+    .required("Please select a brand KPI option"),
+  brandCompetitors: Yup.string().required("Please type in a competitor option"),
+  brandGuidelines: Yup.string().required(
+    "Please type a brand guideline option",
+  ),
+
+  // ATTACH A FILE
+  document: Yup.mixed().nullable().optional(),
+});
