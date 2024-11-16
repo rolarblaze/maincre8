70a91d2b31fd6157da2f61d224d78bc0f760b3d4
ODINKS <nicholas.okeke87@gmail.com>
2024-11-16 11:41:53 +0100

    fix(faq-page): aligned how-we-deliver steps on a faq page

--- components/NewPages/FAQPage/sections/ComprehensiveSupport/index.tsx
+++ components/NewPages/FAQPage/sections/ComprehensiveSupport/index.tsx
@@ -6,9 +6,9 @@ import React from "react";
 function ComprehensiveSupport() {
   return (
     <section className="flex w-full px-0 md:px-[6.25rem] md:py-[6.25rem]">
-      <div className="flex flex-col justify-between gap-5 rounded-3xl bg-grey50 px-8 md:px-12 md:flex-row">
-        <div className="w-full max-w-[500px] flex flex-col gap-y-4 py-12">
-          <h2 className="text-textMain text-[2rem] leading-[2.5rem] md:text-[2.5rem] md:leading-[3rem]">
+      <div className="flex flex-col justify-between gap-5 rounded-3xl bg-grey50 px-8 md:flex-row md:px-12">
+        <div className="flex w-full max-w-[500px] flex-col gap-y-4 py-12">
+          <h2 className="text-[1.5rem] leading-8 text-textMain sm:text-[2rem] sm:leading-[2.5rem] md:text-[2.5rem] md:leading-[3rem]">
             Comprehensive Support on Every Package
           </h2>
           <p className="text-base text-grey600 md:text-lg">
@@ -17,7 +17,11 @@ function ComprehensiveSupport() {
             seamless collaboration, timely deliverables, and tailored solutions
             to meet your creative needs.
           </p>
-          <Button label="Contact Us" classNames="self-start !w-auto" link="/contact-us" />
+          <Button
+            label="Contact Us"
+            classNames="self-start !w-auto py-2 sm:py-4"
+            link="/contact-us"
+          />
         </div>
         <Image
           src={assetLibrary.packageImg}
