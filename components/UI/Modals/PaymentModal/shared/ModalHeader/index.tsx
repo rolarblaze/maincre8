518a4febe707f5de58d5b55134de52cf6297a795
ODINKS <nicholas.okeke87@gmail.com>
2024-11-20 21:10:59 +0100

    design(payment-staus-modal): style modal header

--- components/UI/Modals/PaymentModal/shared/ModalHeader/index.tsx
+++ components/UI/Modals/PaymentModal/shared/ModalHeader/index.tsx
@@ -7,7 +7,15 @@ interface ModalHeaderProps {
 }
 
 function ModalHeader({ icon, title, subtitle }: ModalHeaderProps) {
-  return <div>ModalHeader</div>;
+  return (
+    <header className="space-y-5 border-b border-grey200 pb-6">
+      {icon}
+      <div className="space-y-2">
+        <h4>{title}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </header>
+  );
 }
 
 export default ModalHeader;
