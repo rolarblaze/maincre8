e0eb23e650795886ac845347579433dbec42ba23
ODINKS <nicholas.okeke87@gmail.com>
2024-11-20 22:00:14 +0100

    design(payment-staus-modal): style modal body

--- components/UI/Modals/PaymentModal/shared/ModalBody/index.tsx
+++ components/UI/Modals/PaymentModal/shared/ModalBody/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
+import RedirectLink from "../RedirectLink";
 
 interface ModalBodyProps {
   packageName?: string;
-  price?: string;
+  price?: string | number;
   date?: string;
   redirectLink?: string;
+  showRedirectLink?: boolean;
 }
 
-function ModalBody({ packageName, price, date, redirectLink }: ModalBodyProps) {
-  return <div>ModalBody</div>;
+function ModalBody({
+  packageName = "Graphic Design-Basic | Brand Design-Standard",
+  price = "$499",
+  date = "01 July 2024",
+  redirectLink = "/dashboard/services",
+  showRedirectLink = false,
+}: ModalBodyProps) {
+  return (
+    <main className="w-full space-y-6 py-6">
+      <h5 className="">Order details</h5>
+      <div>
+        <div>
+          <p className="text-sm font-semibold text-grey500">PACKAGE</p>
+          <p className="text-grey700">{packageName}</p>
+        </div>
+        <div className="flex gap-16">
+          <div>
+            <p className="text-sm font-semibold text-grey500">PRICE</p>
+            <p className="text-grey700">{price}</p>
+          </div>
+          <div>
+            <p className="text-sm font-semibold text-grey500">DATE</p>
+            <p className="text-grey700">{date}</p>
+          </div>
+        </div>
+      </div>
+      {showRedirectLink && (
+        <RedirectLink url={redirectLink} linkText="Back to service page" />
+      )}
+    </main>
+  );
 }
 
 export default ModalBody;
