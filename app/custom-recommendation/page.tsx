f545085ab2f40d842d2d30d8196ac31420be6e7f
ODINKS <nicholas.okeke87@gmail.com>
2024-11-28 15:53:31 +0100

    design(custom-recommend): add header and footer

--- app/custom-recommendation/page.tsx
+++ app/custom-recommendation/page.tsx
@@ -1,15 +1,16 @@
+import { PageLayout } from "@/components";
 import WebsiteRecommendForm from "@/components/NewPages/CustomRecommend/WebsiteRecommendForm";
 import WhiteHeroSection from "@/components/NewPages/sharedSections/WhiteHeroSection";
 
 const BusinessForm = () => {
   return (
-    <>
+    <PageLayout>
       <WhiteHeroSection
         title="Let Us Help You Choose the Perfect Package!"
         paragraph="Answer a few questions, and we’ll recommend the best options tailored to your needs."
       />
       <WebsiteRecommendForm />
-    </>
+    </PageLayout>
   );
 };
 export default BusinessForm;
