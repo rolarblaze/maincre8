9cdb6d70a474898e6725ad200867252cad14d20f
rolarblaze <wordofwidom_2@yahoo.com>
2024-12-29 23:33:24 +0100

    Terraform execute ecsTaskExecutePolicy

--- deploy/staging/main.tf
+++ deploy/staging/main.tf
@@ -50,49 +50,26 @@ resource "aws_ecs_task_definition" "app_task" {
 }
 
 # Task Execution Role
+data "aws_iam_policy_document" "github_oidc_role_policy" {
+  statement {
+    actions = [
+      "ecs:CreateCluster",
+      "ecs:DeleteCluster",
+      "ecs:DescribeClusters",
+      "ecs:ListClusters",
+      # Add other necessary permissions
+    ]
+    resources = ["*"]
+  }
+}
+
+
+# Task Execution Role
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-resource "aws_iam_role_policy" "ecs_task_execution_policy" {
-  name   = "ecsTaskExecutionPolicy"
-  role   = aws_iam_role.ecsTaskExecutionRole.name
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "ecs:CreateCluster",
-          "ecs:DescribeClusters",
-          "ecs:ListClusters",
-          "ecs:UpdateCluster"
-        ],
-        "Resource": "*"
-      }
-    ]
-  }
-  EOF
-}
-
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
 
 # Provide a reference to your default VPC
 resource "aws_default_vpc" "default_vpc" {
