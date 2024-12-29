be2b32aa078be8712a2f845da5cf039353fa2f2a
rolarblaze <wordofwidom_2@yahoo.com>
2024-12-29 23:22:32 +0100

    ECS task role changed

--- deploy/production/main.tf
+++ deploy/production/main.tf
@@ -49,29 +49,26 @@ resource "aws_ecs_task_definition" "app_task" {
 }
 
 
+data "aws_iam_policy_document" "github_oidc_role_policy" {
+  statement {
+    actions = [
+      "ecs:CreateCluster",
+      "ecs:DeleteCluster",
+      "ecs:DescribeClusters",
+      "ecs:ListClusters",
+      # Add other necessary permissions
+    ]
+    resources = ["*"]
+  }
+}
+
+
 # Task Execution Role
 resource "aws_iam_role" "ecsTaskExecutionRole" {
   name = "ecsTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-# Assume role policy for ECS tasks
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-# Attach the ECS Task Execution Role policy
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
 # Provide a reference to your default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
